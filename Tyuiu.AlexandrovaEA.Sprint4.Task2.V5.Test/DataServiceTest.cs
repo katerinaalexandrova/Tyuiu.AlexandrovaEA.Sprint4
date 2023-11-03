178dc04171e9245871635a963c97e1f1c6bdb2ca
tyuiu.alexandrovaea@outlook.com <tyuiu.alexandrovaea@outlook.com>
2023-11-03 15:30:13 +0500

    Task2

---
+++ Tyuiu.AlexandrovaEA.Sprint4.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AlexandrovaEA.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.AlexandrovaEA.Sprint4.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[] { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            var res = ds.Calculate(array);
+            Assert.AreEqual(32, res);
+        }
+    }
+}
---
+++ Tyuiu.AlexandrovaEA.Sprint4.Task2.V5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AlexandrovaEA.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.AlexandrovaEA.Sprint4.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            int[] array = new int[14];
+            Console.Title = "Спринт #4 | Выполнил: Александрова Е. А. | ИСПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Александрова Е. А. | ИСПБ-23-1                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный        * ");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 0 до 9 подсчитать сумму четных   *");
+            Console.WriteLine("* элементов массива.  С клавиатуры: 8, 2, 7, 5, 0, 7, 4, 7, 5, 7          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(3, 9);
+            }
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Элемент [" + i + "] - " + array[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(array));
+
+            Console.ReadKey();
+        }
+    }
+}
